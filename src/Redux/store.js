a4383cfc0e56963d3d4830e1618ec5cbd84da9ea
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-03-02 17:06:31 +0200

    save

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore } from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";  // експортує  футкцію яка буде огортати стор
+import { persistStore } from "redux-persist";      // бібліотека яка запамятовує глобальний стан
 
 import rootReducer from './root-reducer'
 
@@ -10,7 +10,9 @@ export const store = configureStore({
 
 export const persistor = persistStore(store)
 
+// Redux Toolkit - це офіційна бібліотека для ефективної розробки з використанням Redux, яка призначена для стандартизації та спрощення написання логіки Redux.
+// Redux Toolkit надає функцію configureStore(options), яка обертає оригінальний createStore(), єдиним аргументом очікує об'єкт параметрів та налаштовує деякі корисні інструменти  
 
-
+//store-це  об'єкт, який містить повний стан програми, методи доступу до стану та відправлення екшенів. У програмі може бути лише один стор. 
 
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react'
-import{store ,persistor} from './Redux/store'
+import{store , persistor} from './Redux/store'
 
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'; // імпортуємо компонент провайдер який дає доступ до глобального стану і передає сторе огортуєм App імаємо глобальний доступ до store
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
